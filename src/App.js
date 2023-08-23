d7e22148bc774ff866d93c8027ad8b94eb2b5804
amr mosbah <aamrmosbah@gmail.com>
2023-08-23 05:06:55 +0300

    Create The Page with Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,254 @@
 
 function App() {
   return (
-    <section>
-        <h1 className="title">Amr Mosbah</h1>
-        <h2>Web Developer</h2>
-    </section>
+    <>
+
+      <header className="header">
+        <a href="#" className="logo">Amr Ahmed</a>
+        <nav className="nav">
+          <a href="#main">Home</a>
+          <a href="#services">services</a>
+          <a href="#projects">projects</a>
+          <a href="#contact">contact</a>
+        </nav>
+      </header>
+
+      <section className="main" id="main">
+        <div className="container">
+          <div className="box">
+            <div className="text">
+              <h1>Hello, I'm Amr Ahmed <br />
+                <span>
+                  Front End Developer <br />
+                  &amp;  UI UX designer
+                </span>
+              </h1>
+              <h2>I Making Pages and Websites</h2>
+              <a href="#projects" className="main_btn">View my work</a>
+              <div className="link">
+                <a href="https://github.com/Amrmosbah22" className="github"><i className="fa-brands fa-github" /></a>
+                <a href="https://www.linkedin.com/in/amr-mosbah-106a16268" className="linkedin"><i className="fa-brands fa-linkedin" /></a>
+                <a href="https://www.facebook.com/amr.mosbah.9803/" className="face"><i className="fa-brands fa-facebook" /></a>
+              </div>
+            </div>
+            <div className="img">
+              <img src="img/frontend.png" alt="photo" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="services" id="services">
+        <div className="container">
+          <h2 className="title">services</h2>
+          <div className="content">
+            <div className="cards">
+              <div className="icon">
+                <i className="fa-solid fa-file-code" />
+              </div>
+              <div className="info">
+                <h3>UI &amp; UX</h3>
+                <p>I can create multiple pages using html and css in the simplest and fastest way.</p>
+              </div>
+            </div>
+            <div className="cards">
+              <div className="icon">
+                <i className="fa-brands fa-square-js" />
+              </div>
+              <div className="info">
+                <h3>Front End</h3>
+                <p>Create a complete website as a front end, and this includes HTML, CSS, JS and ReactJs.</p>
+              </div>
+            </div>
+            <div className="cards">
+              <div className="icon">
+                <i className="fa-solid fa-photo-film" />
+              </div>
+              <div className="info">
+                <h3>Video Editor</h3>
+                <p>Editing photos, videos, and photography as well.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects" id="projects">
+        <div className="container">
+          <h2 className="title">projects</h2>
+          <div className="content">
+            <div className="card">
+              <div className="img">
+                <img src="img/work 1.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Animation landing Page</p>
+                <strong className="project-title">
+                  <span>landing Page</span>
+                  <a href="#">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 2.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>landing page and animations </p>
+                <strong className="project-title">
+                  <span>landing page</span>
+                  <a href="https://amrmosbah22.github.io/LandingPage-2/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 3.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Photographer landing Page</p>
+                <strong className="project-title">
+                  <span>landing Page</span>
+                  <a href="https://amrmosbah22.github.io/Photogarpher/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 4.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Grill restaurant</p>
+                <strong className="project-title">
+                  <span>Restaurant Page</span>
+                  <a href="https://amrmosbah22.github.io/RestaurantPage/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 5.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Calculator landing Page</p>
+                <strong className="project-title">
+                  <span>calculator</span>
+                  <a href="https://amrmosbah22.github.io/MyCalc/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 6.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Word landing Page</p>
+                <strong className="project-title">
+                  <span>Word</span>
+                  <a href="https://amrmosbah22.github.io/My_Word/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 7.jpeg" alt="photo" />
+              </div>
+              <div className="info">
+                <p>landing Page</p>
+                <strong className="project-title">
+                  <span>Classes</span>
+                  <a href="https://amrmosbah22.github.io/ClassPage/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 8.jpeg" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Buildings Page</p>
+                <strong className="project-title">
+                  <span>Apex</span>
+                  <a href="https://amrmosbah22.github.io/ApexPage/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 9.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Product Management</p>
+                <strong className="project-title">
+                  <span>Cruds</span>
+                  <a href="https://amrmosbah22.github.io/ProductApp/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 10.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>Shop Watches</p>
+                <strong className="project-title">
+                  <span>Time Zone</span>
+                  <a href="https://amrmosbah22.github.io/WatchPage/">More Details</a>
+                </strong>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="img/work 12.png" alt="photo" />
+              </div>
+              <div className="info">
+                <p>X O Game</p>
+                <strong className="project-title">
+                  <span>Game</span>
+                  <a href="https://amrmosbah22.github.io/XO_Game/">More Details</a>
+                </strong>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="contact" id="contact">
+        <div className="container">
+          <h2 className="title">Let's work together</h2>
+          <div className="content">
+            <div className="cards">
+              <div className="icon">
+                <i className="fa-solid fa-phone" />
+              </div>
+              <div className="info">
+                <h3>Phone</h3>
+                <p>+201111491426</p>
+              </div>
+            </div>
+            <div className="cards">
+              <div className="icon">
+                <i className="fa-solid fa-envelope" />
+              </div>
+              <div className="info">
+                <h3>Email</h3>
+                <p>aamrmosbah@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="footer">
+        <div className="container">
+          <p>Copyright@<span>Amr Ahmed</span></p>
+          <div className="link">
+            <a href="https://github.com/Amrmosbah22" className="github"><i className="fa-brands fa-github" /></a>
+            <a href="https://www.linkedin.com/in/amr-mosbah-106a16268" className="linkedin"><i className="fa-brands fa-linkedin" /></a>
+            <a href="https://www.facebook.com/amr.mosbah.9803/" className="face"><i className="fa-brands fa-facebook" /></a>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
 
